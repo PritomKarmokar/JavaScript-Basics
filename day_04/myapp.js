8ebde0590fb267a516ddbbacf799de66db62882c
Pritom <pritomkarmokar99@gmail.com>
2022-11-05 01:32:23 +0600

    modifying day_04

--- day_04/myapp.js
+++ day_04/myapp.js
@@ -1,9 +1,26 @@
 // console.log( document.getElementById('hello') );
 
-pHello = document.getElementById('hello');
+// If we write console.dir() it'll print an interactive list so we can view the object that actually is.
+
+// console.dir( document.getElementById( 'hello' ));
+
+//document.getElementById( 'hello' ).innerHTML += " order";
+
+var pHello = document.getElementById('hello');
+
 pHello.innerText = "new world";
-pHello.innerHTML += " order <span>hello world</span>"
+
+// pHello.innerText += " order <span>hello world</span>";
+// The above statement will simply put the Text inside the quotation where <span> tags are also included.
+
+pHello.innerHTML += " order <span>Hello world</span>"
+// But the 'innerHTML' will allow us to create html content in the actual element itself.
+
+pHello.outerHTML = '<h2 id="hello">new world order <span>hello world</span></p>';
 
 var spanH1 = document.querySelectorAll('h1 span');
+// This returns an array so we can't use innerHTML type of things. instead we'll be using array type indexing.
 
-spanH1[0].innerHTML = "new text here!!";
+var spanH2 = document.querySelectorAll('h1 span')[0]
+
+spanH2.innerHTML = "new text here!!";
