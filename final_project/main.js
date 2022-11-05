7eb6aa7306c18c060b2f19bdc195eb5b675dedb2
Pritom <pritomkarmokar99@gmail.com>
2022-11-05 17:50:42 +0600

    final project added

---
+++ final_project/main.js
@@ -0,0 +1,25 @@
+let elements = document.getElementsByName('cssProperty');
+let div = document.getElementById('modify');
+
+function set()
+{
+    // let elements = document.getElementsByName('cssProperty');
+    // alert('clicked');
+
+    console.log( elements );
+
+    for( let index = 0; index < elements.length; index++){
+        
+        let cssProperty = elements[index].getAttribute('id') ;
+        
+        let cssValue = elements[index].value ;
+
+        // console.log( cssProperty, cssValue);
+        
+
+        div.style[ cssProperty ] = cssValue;
+
+    }
+}
+
+document.getElementById('set').addEventListener('click', set);
